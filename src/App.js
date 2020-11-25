a171f0d028eb9abdda6729f055ce6e22b33ba0b7
Manuel Pierre <pierrem1.mp@gmail.com>
2020-11-25 03:17:25 -0500

    corrected about routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div>
       <Router>
         <Header />
-        <Route path={["/", "/about"]} component={About} />
+        <Route exact path={["/", "/about"]} component={About} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Footer />
       </Router>
